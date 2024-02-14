5e32a75f6e9edb4dab495b3737e3b7d55c2aaffd
Christian Bouwens <christian.bouwens@genomicsengland.co.uk>
2024-02-14 14:36:35 +0000

    rewrote running the script

--- run_dndscv.R
+++ run_dndscv.R
@@ -31,8 +31,7 @@ p <- add_argument(
 p <- add_argument(
     p,
     "--known_cancer_genes", type="character",
-    help = 'List of a-priori known cancer genes (to be excluded from the indel background model) - references the cancer gene census v81.  (cancergenes_cgc81.rda)',
-    default ="cgc81"
+    help = 'List of a-priori known cancer genes (to be excluded from the indel background model) - references the cancer gene census v81.  ("cgc81" = cancergenes_cgc81.rda)'
 )
 p <- add_argument(
     p,
@@ -99,36 +98,43 @@ args <- parse_args(p)
 
 #### Wrangle input #####
 library("dndscv")
-
+library(data.table)
 # data("dataset_simbreast", package="dndscv") # Example dataset
 # dndsout = dndscv(mutations, refdb="example_output_refcds.rda", cv=NULL)
 if (args$covariates == 'covariates_hg19_hg38_epigenome_pcawg.rda') {
-    load(args$covariates)
-    args$covariates = covs
+    load('/usr/src/app/data/covariates_hg19_hg38_epigenome_pcawg.rda')
+    covs <- covs
+} else {
+    covs <- args$covariates
 }
 
+
+# read in table
+mutations <- fread(muts, sep='\t', header=TRUE)
+# mutations <- mutations[,.(SAMPLE,CHROM,POS, REF,ALT)] # Restricting input matrix to first 5 columns
+mutations <- mutations[,.(CHROM,POS, REF,ALT,SAMPLE)]
 dndsout <- dndscv(
-    args$input,  # the aggregate variantfile
+    mutations,  # the aggregate variantfile
     gene_list = NULL, # limit analysis to this gene list (vector)
     refdb = args$ref, # reference file to use
     sm = args$subm,  # Substitution model (precomputed models are available in the data directory) (submod_192r_3w.rda) )12r and 2r are available too.
-    kc = args$kc,  # List of a-priori known cancer genes (to be excluded from the indel background model) - references the cancer gene census v81.  (cancergenes_cgc81.rda)
-    cv = args$covariates, # Covariates (a matrix of covariates -columns- for each gene -rows-) [default: reference covariates] [cv=NULL runs dndscv without covariates] ()
+    kc = args$known_cancer_genes,  # List of a-priori known cancer genes (to be excluded from the indel background model) - references the cancer gene census v81.  (cancergenes_cgc81.rda)
+    cv = covs, # Covariates (a matrix of covariates -columns- for each gene -rows-) [default: reference covariates] [cv=NULL runs dndscv without covariates] ()
     max_muts_per_gene_per_sample = args$maxmuts,  # If n<Inf, arbitrarily the first n mutations by chr position will be kept 
-    max_coding_muts_per_sample = args$maxtmb,  # to filter out hypermutated samples.
+    max_coding_muts_per_sample = args$tbmmax,  # to filter out hypermutated samples.
     use_indel_sites = args$indels, # Use unique indel sites instead of the total number of indels (it tends to be more robust)
     min_indels = args$mindels, #  Minimum number of indels required to run the indel recurrence module
-    maxcovs = args$maxcov, # Maximum number of covariates that will be considered (additional columns in the matrix of covariates will be excluded)
-    constrain_wnon_wspl = args$constrain, # This constrains wnon==wspl (this typically leads to higher power to detect selection) nonsense=wnon, missense=wmis
-    outp = args$output_type,# Output: 1 = Global dN/dS values; 2 = Global dN/dS and dNdSloc; 3 = Global dN/dS, dNdSloc and dNdScv
+    maxcovs = args$amaxcov, # Maximum number of covariates that will be considered (additional columns in the matrix of covariates will be excluded)
+    constrain_wnon_wspl = args$wnon_constrain, # This constrains wnon==wspl (this typically leads to higher power to detect selection) nonsense=wnon, missense=wmis
+    outp = args$loc_sv_type,# Output: 1 = Global dN/dS values; 2 = Global dN/dS and dNdSloc; 3 = Global dN/dS, dNdSloc and dNdScv
     numcode = args$gennum, # NCBI genetic code number (default = 1; standard genetic code). To see the list of genetic codes supported use: ? seqinr::translate. Note that the same genetic code must be used in the dndscv and buildref functions.
     outmats = FALSE, # Output the internal N and L matrices (default = F)
-    mingenecovs = 500  # Minimum number of genes required to run the negative binomial regression model with covariates (default = 500), 
+    mingenecovs = 500,  # Minimum number of genes required to run the negative binomial regression model with covariates (default = 500), 
     dc = NULL)
 
 write.table(
     dndsout$sel_cv, 
-    file=paste0(args$outdir, '/', args$runname),
+    file=paste0(args$out, '/', args$name),
     sep='\t', 
     quote=FALSE
     )
@@ -139,7 +145,7 @@ write.table(
     # change line of code to search for hg38 in the data folder (place there when downloaded)
 # how can we input other refcds files? is it the path to the .rda file?
     # (if so place into the data folder + allow specified paths.)
-# are the sm, kc and cb included in the hg38 download?
+
 # max_muts_per_gene_per_sample how does this affect the output? is it limited to non-synonymous muts only? (probably to filter false positives / repeats?)
 
 
