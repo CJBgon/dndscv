ee1dc626b61e93cc4919b5bf6707a2115a5a8cb9
Christian Bouwens <48950814+CJBgon@users.noreply.github.com>
2024-06-27 13:47:03 +0100

    Update run_dndscv.R

--- run_dndscv.R
+++ run_dndscv.R
@@ -4,109 +4,88 @@ suppressPackageStartupMessages(library("argparser"))
 
 
 ######### input arguments #############
-p <- arg_parser("identify coding genes under positive selection")
+parser <- ArgumentParser()
 
-p <- add_argument(
-    p,
-    "--varagg", type="character",
+parser$add_argument(
+    "-f", "--input", type="character",
     help ='Path to the variant aggregate.'
 )
-p <- add_argument(
-    p,
-    "--out", type="character", default='/out/',
+parser$add_argument(
+    "-o", "--out", type="character", default='/out/',
     help = 'Path to the output directory.'
 )
-p <- add_argument(
-    p,
-    "--ref", type="character",
+parser$add_argument(
+    "-r", "--ref", type="character",
     help = '"hg38, hg19: or path to the reference cds file.',
     default = "hg19"
 )
-p <- add_argument(
-    p,
-    "--subm", type="character",
+parser$add_argument(
+    "-s", "--subm", type="character",
     help = 'Substitution model (precomputed models are available in the data directory) (submod_192r_3w.rda,12r and 2r are available too.)',
     default ="192r_3w"
 )
 p <- add_argument(
     p,
-    "--known_cancer_genes", type="character",
-    help = 'List of a-priori known cancer genes (to be excluded from the indel background model) - references the cancer gene census v81.  ("cgc81" = cancergenes_cgc81.rda)'
+    "--known_cancer_genes", type="character", nargs='+',
+    help = 'List of a-priori known cancer genes (to be excluded from the indel background model) - references the cancer gene census v81.  ("cgc81" = cancergenes_cgc81.rda)',
+    default = "cgc81"
 )
-p <- add_argument(
-    p,
-    "--covariates", type="character",
+parser$add_argument(
+    "-c", "--covariates", type="character",
     help = 'Covariates (a matrix of covariates -columns- for each gene -rows-) [default: reference covariates] [cv=NULL runs dndscv without covariates] (hg19_hg38_epigenome_pcawg.rda)',
     default ="hg19"
 )
-p <- add_argument(
-    p,
-    "--maxmuts", type="integer",
+parser$add_argument(
+    "-m", "--maxmuts", type="integer",
     help = 'If n<Inf, arbitrarily the first n mutations by chr position will be kept',
     default = 3
 )
-p <- add_argument(
-    p,
-    "--tmbmax", type="integer",
+parser$add_argument(
+    "-t", "--maxtmb", type="integer",
     help = 'to filter out hypermutated samples.',
     default = 3000
 )
-p <- add_argument(
-    p,
-    "--indels", flag=TRUE,
+parser$add_argument(
+    "-i", "--indels", action="store_true", default=FALSE,
     help = 'Use unique indel sites instead of the total number of indels (it tends to be more robust)',
 )
-p <- add_argument(
-    p,
-    "--mindels", type="integer",
+parser$add_argument(
+    "-p", "--mindels", type="integer",
     help = 'Minimum number of indels required to run the indel recurrence module.',
     default = 5
 )
-p <- add_argument(
-    p, 
-    "--amaxcov", type="integer",
+parser$add_argument(
+    "-a", "--maxcov", type="integer",
     help = 'Maximum number of covariates that will be considered (additional columns in the matrix of covariates will be excluded)',
     default = 20
 )
-p <- add_argument(
-    p,
-    "--wnon_constrain", flag=TRUE,
+parser$add_argument(
+    "-w", "--constrain", action="store_false", default=TRUE
     help = 'This constrains wnon==wspl (this typically leads to higher power to detect selection)',
 )
-p <- add_argument(
-    p,
-    "--loc_sv_type", type="integer", default=3,
+parser$add_argument(
+    "-d", "--output_type", type="integer", default=3
     help = 'Output: 1 = Global dN/dS values; 2 = Global dN/dS and dNdSloc; 3 = Global dN/dS, dNdSloc and dNdScv',
 )
-p <- add_argument(
-    p,
-    "--gennum", type="integer", default=1,
+parser$add_argument(
+    "-g", "--gennum", type="integer", default=1
     help = 'NCBI genetic code number (default = 1; standard genetic code). To see the list of genetic codes supported use: ? seqinr::translate. Note that the same genetic code must be used in the dndscv and buildref functions.',
 )
-p <- add_argument(
-    p,
-    "--name", type="character",
+parser$add_argument(
+    "-n", "--runname", type="character",
     help = 'name of the run - given to output file',
     default='dndscv'
 )
-p <- add_argument(
-    p,
-    "--genelist", type="character", nargs=Inf,
-    help = 'limit the run to this vector of genes',
-    default=NULL
-)
-p <- add_argument(
-    p,
-    "--mingenecovs", type="integer",
-    help = 'Minimum number of genes required to run the negative binomial regression model with covariates (default = 500)',
-    default=500
-)
+# parser$add_argument(
+#     "-v", "--genelist", type="character", nargs='+',
+#     help = 'limit the run to this vector of genes'
+# )
 
-args <- parse_args(p)
+args <- parser$parse_args()
 
 #### Wrangle input #####
 library("dndscv")
-library(data.table)
+
 # data("dataset_simbreast", package="dndscv") # Example dataset
 # dndsout = dndscv(mutations, refdb="example_output_refcds.rda", cv=NULL)
 if (args$covariates == 'covariates_hg19_hg38_epigenome_pcawg.rda') {
@@ -116,6 +95,19 @@ if (args$covariates == 'covariates_hg19_hg38_epigenome_pcawg.rda') {
     covs <- args$covariates
 }
 
+# solving an issue with arparser not taking optional flag and character inputs simultaneously.
+# genelist: Remove blank spaces and split gene names into vector
+args$genelist <- unlist(strsplit(gsub(" ", "", args$genelist), split = ",", fixed = TRUE))
+
+# known_cancer_genes: Remove blank spaces and split gene names into vector
+args$known_cancer_genes <- unlist(strsplit(gsub(" ", "", args$known_cancer_genes), split = ",", fixed = TRUE))
+
+# Assign default value to genelist as NULL, as required by the dndscv function.
+if((length(args$genelist) == 1)){
+    if(toupper(args$genelist) == "NONE"){
+        args$genelist <- NULL
+    }
+}
 
 # read in table
 mutations <- fread(args$varagg, sep='\t', header=TRUE)
@@ -125,28 +117,28 @@ mutations <- mutations[,.(CHROM,POS, REF,ALT,SAMPLE)]
 setDT(mutations)[, CHROM := gsub("\\chr", "",x=CHROM)]
 
 dndsout <- dndscv(
-    mutations,  # the aggregate variantfile
-    gene_list = args$genelist, # limit analysis to this gene list (vector)
+    args$input,  # the aggregate variantfile
+    gene_list = NULL, # limit analysis to this gene list (vector)
     refdb = args$ref, # reference file to use
     sm = args$subm,  # Substitution model (precomputed models are available in the data directory) (submod_192r_3w.rda) )12r and 2r are available too.
-    kc = args$known_cancer_genes,  # List of a-priori known cancer genes (to be excluded from the indel background model) - references the cancer gene census v81.  (cancergenes_cgc81.rda)
-    cv = covs, # Covariates (a matrix of covariates -columns- for each gene -rows-) [default: reference covariates] [cv=NULL runs dndscv without covariates] ()
+    kc = args$kc,  # List of a-priori known cancer genes (to be excluded from the indel background model) - references the cancer gene census v81.  (cancergenes_cgc81.rda)
+    cv = args$covariates, # Covariates (a matrix of covariates -columns- for each gene -rows-) [default: reference covariates] [cv=NULL runs dndscv without covariates] ()
     max_muts_per_gene_per_sample = args$maxmuts,  # If n<Inf, arbitrarily the first n mutations by chr position will be kept 
-    max_coding_muts_per_sample = args$tbmmax,  # to filter out hypermutated samples.
+    max_coding_muts_per_sample = args$maxtmb,  # to filter out hypermutated samples.
     use_indel_sites = args$indels, # Use unique indel sites instead of the total number of indels (it tends to be more robust)
     min_indels = args$mindels, #  Minimum number of indels required to run the indel recurrence module
-    maxcovs = args$amaxcov, # Maximum number of covariates that will be considered (additional columns in the matrix of covariates will be excluded)
-    constrain_wnon_wspl = args$wnon_constrain, # This constrains wnon==wspl (this typically leads to higher power to detect selection) nonsense=wnon, missense=wmis
-    outp = args$loc_sv_type,# Output: 1 = Global dN/dS values; 2 = Global dN/dS and dNdSloc; 3 = Global dN/dS, dNdSloc and dNdScv
+    maxcovs = args$maxcov, # Maximum number of covariates that will be considered (additional columns in the matrix of covariates will be excluded)
+    constrain_wnon_wspl = args$constrain, # This constrains wnon==wspl (this typically leads to higher power to detect selection) nonsense=wnon, missense=wmis
+    outp = args$output_type,# Output: 1 = Global dN/dS values; 2 = Global dN/dS and dNdSloc; 3 = Global dN/dS, dNdSloc and dNdScv
     numcode = args$gennum, # NCBI genetic code number (default = 1; standard genetic code). To see the list of genetic codes supported use: ? seqinr::translate. Note that the same genetic code must be used in the dndscv and buildref functions.
     outmats = FALSE, # Output the internal N and L matrices (default = F)
-    mingenecovs = args$mingenecovs,  # Minimum number of genes required to run the negative binomial regression model with covariates (default = 500), 
-    dc = NULL) # Duplex coverage per gene. Named Numeric Vector with values reflecting the mean duplex coverage per site per gene, and names corresponding to gene names. Use this argument only when running dNdScv on duplex sequencing data to use gene coverage in the offset of the regression model 
+    mingenecovs = 500  # Minimum number of genes required to run the negative binomial regression model with covariates (default = 500), 
+    dc = NULL)
 
 write.table(
-    dndsout$sel_cv,
-    file=paste0(args$out, '/', args$name),
-    sep='\t',
+    dndsout$sel_cv, 
+    file=paste0(args$outdir, '/', args$runname),
+    sep='\t', 
     quote=FALSE
     )
 
@@ -156,7 +148,7 @@ write.table(
     # change line of code to search for hg38 in the data folder (place there when downloaded)
 # how can we input other refcds files? is it the path to the .rda file?
     # (if so place into the data folder + allow specified paths.)
-
+# are the sm, kc and cb included in the hg38 download?
 # max_muts_per_gene_per_sample how does this affect the output? is it limited to non-synonymous muts only? (probably to filter false positives / repeats?)
 
 
